6aa808d64be5b7c61ad472e8f63b9efee2ec0a80
blackpenpress <contact@blackpenpress.co.uk>
2025-04-30 10:52:49 +0100

    update service provider

--- src/FilamentSpatieMediaLibraryCroppieServiceProvider.php
+++ src/FilamentSpatieMediaLibraryCroppieServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace JosefBehr\FilamentSpatieMediaLibraryCroppie;
 
-use Filament\PluginServiceProvider;
 use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Filament\Support\Assets\AlpineComponent;
+use Filament\Support\Assets\Asset;
+use Filament\Support\Assets\Css;
+use Filament\Support\Facades\FilamentAsset;
 
-class FilamentSpatieMediaLibraryCroppieServiceProvider extends PluginServiceProvider
+class FilamentSpatieMediaLibraryCroppieServiceProvider extends PackageServiceProvider
 {
     public static string $name = 'filament-spatie-media-library-croppie';
 
@@ -25,5 +29,14 @@ class FilamentSpatieMediaLibraryCroppieServiceProvider extends PluginServiceProv
             ->hasAssets()
             ->hasViews('filament-spatie-media-library-croppie')
             ->hasTranslations();
+    }
+
+    public function packageBooted(): void
+    {
+        // Register assets
+        FilamentAsset::register([
+            Css::make('filament-spatie-media-library-croppie-style', __DIR__ . '/../resources/dist/css/filament-spatie-media-library-croppie.css'),
+            AlpineComponent::make('filament-spatie-media-library-croppie-component', __DIR__ . '/../resources/dist/js/filament-spatie-media-library-croppie.js'),
+        ]);
     }
 }
